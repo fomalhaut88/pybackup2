335c7ac9f30df5488b81ff8926ab79cc998e7e2a
alexander <mail@fomalhaut.su>
2018-12-26 14:57:09 +0300

    Release 1.1. Fixed linux service.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ./build-ubuntu64.sh
 sudo dpkg --remove pybackup2
-sudo dpkg -i dist/pybackup2-ubuntu64-1.0.deb
+sudo dpkg -i dist/pybackup2-ubuntu64-1.1.deb
--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,13 @@ import os
 import sys
 
 
-CONFIG_PATH = os.path.abspath(".pybackup2")
+try:
+    os.getlogin()
+except:
+    CONFIG_PATH = os.path.abspath(".pybackup2")
+else:
+    CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".pybackup2")
+
 BASE_DIR = sys._MEIPASS if getattr(sys, 'frozen', False) else '.'
 
 
